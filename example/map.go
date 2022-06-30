a4766053775fb104b2a5424af4d439ebacf13c23
jianjustin <janine.jian.chen@gmail.com>
2022-06-30 08:05:37 +0800

    add map example

---
+++ example/map.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	//创建字典
+	m := make(map[string]int)
+	m["k1"] = 7
+	m["k2"] = 13
+	fmt.Println("map:",m)
+
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+	fmt.Println("ken: ", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map: ", m)
+
+	//prs返回字典是否存在值标识
+	_, prs := m["k2"]
+	fmt.Println("prs: ", prs)
+
+}
