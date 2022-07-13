d6379fb937753c702525350b112746fd1f2697f8
jianjustin <janine.jian.chen@gmail.com>
2022-07-13 08:16:30 +0800

    add goruntines example

---
+++ example/goroutines.go
@@ -0,0 +1,9 @@
+package example
+
+import "fmt"
+
+func f(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(i, ":", s)
+	}
+}
---
+++ example/goroutines_test.go
@@ -0,0 +1,15 @@
+package example
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestF(t *testing.T) {
+	go f("goroutines1")
+	go f("goroutines2")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
