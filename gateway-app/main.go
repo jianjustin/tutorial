0afea4d48b782237c394b160158dfa4ad750bb8e
jianjustin <janine.jian.chen@gmail.com>
2023-02-11 22:56:09 +0800

    add grpc restful api

---
+++ gateway-app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	helloworldpb "go.guide/gateway-app/proto/helloworld"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	helloworldpb.UnimplementedGreeterServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+}
+
+func main() {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+	// Attach the Greeter service to the server
+	helloworldpb.RegisterGreeterServer(s, &server{})
+	// Serve gRPC server
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+}
