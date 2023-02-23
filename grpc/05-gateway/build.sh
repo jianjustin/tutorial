b3300dc86c19bd66addf354764783eacd6abbdee
jianjustin <janine.jian.chen@gmail.com>
2023-02-23 22:49:31 +0800

    add grpc gateway example

---
+++ grpc/05-gateway/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+protoc --proto_path=./proto \
+    --go_out=./proto --go_opt=paths=source_relative \
+    --go-grpc_out=./proto --go-grpc_opt=paths=source_relative \
+    --grpc-gateway_out=./proto --grpc-gateway_opt=paths=source_relative \
+    proto/helloworld/helloworld.proto
---
+++ grpc/05-gateway/server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go.guide/gateway-app/proto/helloworld"
+	pb "go.guide/grpc/05-gateway/proto/helloworld"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+)
+
+var (
+	port    = flag.Int("port", 50051, "The server port")
+	restful = flag.Int("restful", 8080, "the port to restful serve on")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	helloworld.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &helloworld.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	helloworld.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	conn, err := grpc.Dial(
+		"localhost:50051",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = pb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *restful),
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0" + fmt.Sprintf(":%d", *restful))
+	log.Fatalln(gwServer.ListenAndServe())
+}
