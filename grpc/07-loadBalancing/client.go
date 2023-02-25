048d2dbebeeaadfbf43dead17a43645172cd526e
jianjustin <janine.jian.chen@gmail.com>
2023-02-25 11:11:03 +0800

    add loadbalancing example

---
+++ grpc/07-loadBalancing/client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	pb "go.guide/grpc/06-nameResolving/proto/helloworld"
+	"google.golang.org/grpc"
+
+	"google.golang.org/grpc/resolver"
+)
+
+const (
+	exampleScheme      = "x"
+	exampleServiceName = "lb.x"
+)
+
+var addrs1 = []string{"localhost:50051", "localhost:50052"}
+
+func callSayHello(c pb.GreeterClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: message})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	fmt.Println(r.Message)
+}
+
+func makeRPCs(cc *grpc.ClientConn, n int) {
+	hwc := pb.NewGreeterClient(cc)
+	for i := 0; i < n; i++ {
+		callSayHello(hwc, fmt.Sprintf("%d", i))
+	}
+}
+
+func main() {
+	// Make another ClientConn with round_robin policy.
+	roundrobinConn, err := grpc.Dial(
+		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // This sets the initial balancing policy.
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer roundrobinConn.Close()
+	makeRPCs(roundrobinConn, 10)
+}
+
+// Following is an example name resolver implementation. Read the name
+// resolution example to learn more about it.
+
+type exampleResolverBuilder struct{}
+
+func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &exampleResolver{
+		target: target,
+		cc:     cc,
+		addrsStore: map[string][]string{
+			exampleServiceName: addrs1,
+		},
+	}
+	r.start()
+	return r, nil
+}
+func (*exampleResolverBuilder) Scheme() string { return exampleScheme }
+
+type exampleResolver struct {
+	target     resolver.Target
+	cc         resolver.ClientConn
+	addrsStore map[string][]string
+}
+
+func (r *exampleResolver) start() {
+	addrStrs := r.addrsStore[r.target.Endpoint()]
+	addrs := make([]resolver.Address, len(addrStrs))
+	for i, s := range addrStrs {
+		addrs[i] = resolver.Address{Addr: s}
+	}
+	r.cc.UpdateState(resolver.State{Addresses: addrs})
+}
+func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
+func (*exampleResolver) Close()                                  {}
+
+func init() {
+	resolver.Register(&exampleResolverBuilder{})
+}
--- order-service-app/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-protoc --go_out=./proto --go_opt=paths=source_relative \
-  --proto_path=./proto \
-  --go-grpc_opt=paths=source_relative \
-  --go-grpc_out=require_unimplemented_servers=false:./proto \
-  proto/*.proto
