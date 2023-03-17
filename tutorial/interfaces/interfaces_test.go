d519a8c996c045bc3b894fe6f75c5825cb5918a7
jianjustin <janine.jian.chen@gmail.com>
2023-03-18 07:28:56 +0800

    feat：add byte_counter_test

--- tutorial/interfaces/interfaces_test.go
+++ tutorial/interfaces/interfaces_test.go
@@ -20,3 +20,32 @@ func TestCircle(t *testing.T) {
 		t.Error("circle error")
 	}
 }
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	weight, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.weight * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.weight + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
