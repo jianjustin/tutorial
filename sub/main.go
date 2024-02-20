8dcbb11d020b7c6e9bd7c32e93c7943bd3ec47c8
jianjustin <janine.jian.chen@gmail.com>
2024-02-20 14:24:15 +0800

    feat: use etcd registry

--- sub/main.go
+++ sub/main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
+	"github.com/go-micro/plugins/v4/registry/etcd"
 	"github.com/jianjustin/sub/handler"
 	"github.com/jianjustin/sub/proto/mul"
 	pb "github.com/jianjustin/sub/proto/sub"
+	"go-micro.dev/v4/registry"
 	"go-micro.dev/v4/server"
 
 	"go-micro.dev/v4"
 	"go-micro.dev/v4/logger"
 
 	grpcc "github.com/go-micro/plugins/v4/client/grpc"
-	_ "github.com/go-micro/plugins/v4/registry/kubernetes"
 	grpcs "github.com/go-micro/plugins/v4/server/grpc"
 )
 
 var (
-	service = "sub"
-	version = "latest"
-	address = ":60002"
+	service      = "sub"
+	version      = "latest"
+	address      = ":60002"
+	etcd_address = "etcd-service:2379"
 )
 
 func main() {
+	etcdRegistry := etcd.NewRegistry(
+		registry.Addrs(etcd_address),
+	)
 
 	// Create service
 	srv := micro.NewService(
 		micro.Server(grpcs.NewServer(server.Address(address))),
 		micro.Client(grpcc.NewClient()),
+		micro.Registry(etcdRegistry),
 	)
 
 	srv.Init(
