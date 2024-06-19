73e935bb24e24bff9cad17ac2e321e73c4c587cd
jianjustin <janine.jian.chen@gmail.com>
2024-06-19 17:31:47 +0800

    Revert "feat: delete google proto"
    This reverts commit c6ad73b85a5f8fe642d16c1a173988156ea9b91f.

--- microservices-app/proto/a/a.pb.gw.go
+++ microservices-app/proto/a/a.pb.gw.go
@@ -35,7 +35,11 @@ func request_A_AddAPreffix_0(ctx context.Context, marshaler runtime.Marshaler, c
 	var protoReq ARequest
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -48,7 +52,11 @@ func local_request_A_AddAPreffix_0(ctx context.Context, marshaler runtime.Marsha
 	var protoReq ARequest
 	var metadata runtime.ServerMetadata
 
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -94,21 +102,21 @@ func RegisterAHandlerServer(ctx context.Context, mux *runtime.ServeMux, server A
 // RegisterAHandlerFromEndpoint is same as RegisterAHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.NewClient(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
