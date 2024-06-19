631cb8c36a845bd9d9a23b933fcdf5f3bdd8d5e3
jianjustin <janine.jian.chen@gmail.com>
2024-06-19 11:38:33 +0800

    feat: add statemachine-go

--- statemachine/go.mod
+++ statemachine/go.mod
@@ -1,3 +1,5 @@
 module go.guide/statemachine
 
 go 1.21.5
+
+require github.com/Gurpartap/statemachine-go v0.0.0-20190708030151-b27cddb816c4 // indirect
---
+++ statemachine/statemachine_test.go
@@ -0,0 +1,111 @@
+package statemachine
+
+import (
+	"github.com/Gurpartap/statemachine-go"
+	"log"
+	"testing"
+)
+
+var processStates = []string{
+	"unmonitored", "running", "stopped",
+	"starting", "stopping", "restarting",
+}
+
+type ExampleProcess struct {
+	statemachine.Machine
+
+	IsAutoStartOn    bool
+	IsProcessRunning bool
+}
+
+func (p *ExampleProcess) GetIsAutoStartOn() bool {
+	return p.IsAutoStartOn
+}
+
+func (p *ExampleProcess) GetIsProcessRunning() bool {
+	return p.IsProcessRunning
+}
+
+func (ExampleProcess) Start() {
+	log.Println("Starting")
+}
+func (ExampleProcess) Stop()    {}
+func (ExampleProcess) Restart() {}
+
+func (ExampleProcess) NotifyTriggers(transition statemachine.Transition) {
+	log.Println("NotifyTriggers", transition)
+}
+func (ExampleProcess) RecordTransition(transition statemachine.Transition) {
+	log.Println(transition)
+}
+
+func (ExampleProcess) LogFailure(err error) {
+	log.Println(err)
+}
+
+func TestForProcess(t *testing.T) {
+	process := &ExampleProcess{}
+
+	process.Machine = statemachine.BuildNewMachine(func(machineBuilder statemachine.MachineBuilder) {
+		machineBuilder.States(processStates...)
+		machineBuilder.InitialState("unmonitored")
+
+		machineBuilder.Event("tick", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().From("starting").To("running").If(process.GetIsProcessRunning)
+			eventBuilder.Transition().From("starting").To("stopped").Unless(process.GetIsProcessRunning)
+
+			// The process failed to die after entering the stopping state.
+			// Change the state to reflect reality.
+			eventBuilder.Transition().From("running").To("stopped").Unless(process.GetIsProcessRunning)
+
+			eventBuilder.Transition().From("stopping").To("running").If(process.GetIsProcessRunning)
+			eventBuilder.Transition().From("stopping").To("stopped").Unless(process.GetIsProcessRunning)
+
+			eventBuilder.Transition().From("stopped").To("running").If(process.GetIsProcessRunning)
+			eventBuilder.Transition().From("stopped").To("starting").If(func(transition statemachine.Transition) bool {
+				return process.GetIsAutoStartOn() && !process.GetIsProcessRunning()
+			})
+
+			eventBuilder.Transition().From("restarting").To("running").If(process.GetIsProcessRunning)
+			eventBuilder.Transition().From("restarting").To("stopped").Unless(process.GetIsProcessRunning)
+		})
+
+		machineBuilder.Event("monitor", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().From("unmonitored").To("stopped")
+		})
+
+		machineBuilder.Event("start", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().From("unmonitored", "stopped").To("starting")
+		})
+
+		machineBuilder.Event("stop", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().From("running").To("stopping")
+		})
+
+		machineBuilder.Event("restart", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().From("running", "stopped").To("restarting")
+		})
+
+		machineBuilder.Event("unmonitor", func(eventBuilder statemachine.EventBuilder) {
+			eventBuilder.Transition().FromAny().To("unmonitored")
+		})
+
+		machineBuilder.BeforeTransition().To("starting").Do(func() { process.IsAutoStartOn = true })
+		machineBuilder.AfterTransition().To("starting").Do(func() { process.Start() })
+
+		machineBuilder.BeforeTransition().To("stopping").Do(func() { process.IsAutoStartOn = false })
+		machineBuilder.AfterTransition().To("stopping").Do(func() { process.Stop() })
+
+		machineBuilder.BeforeTransition().To("restarting").Do(func() { process.IsAutoStartOn = true })
+		machineBuilder.AfterTransition().To("restarting").Do(func() { process.Restart() })
+
+		machineBuilder.BeforeTransition().To("unmonitored").Do(func() { process.IsAutoStartOn = false })
+
+		machineBuilder.BeforeTransition().Any().Do(process.NotifyTriggers)
+		machineBuilder.AfterTransition().Any().Do(process.RecordTransition)
+
+		machineBuilder.AfterFailure().OnAnyEvent().Do(process.LogFailure)
+	})
+
+	process.Fire("start")
+}
