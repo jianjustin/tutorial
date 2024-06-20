b79b6a3ce4c42ecdf42769fede3a6288eb3f8918
jianjustin <janine.jian.chen@gmail.com>
2024-06-20 11:59:14 +0800

    fix: add gRPC services

---
+++ apps/add-service/go.mod
@@ -0,0 +1,35 @@
+module go.guide/add-grpc-service
+
+go 1.21
+
+toolchain go1.21.5
+
+require (
+	github.com/go-kit/kit v0.13.0
+	github.com/go-kit/log v0.2.1
+	github.com/gorilla/mux v1.8.1
+	github.com/opentracing/opentracing-go v1.2.0
+	golang.org/x/sync v0.7.0
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.14 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240617180043-68d350f18fd4 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
+)
---
+++ apps/add-service/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/go-kit/kit/sd/etcdv3"
+	"github.com/go-kit/log"
+	opentracinggo "github.com/opentracing/opentracing-go"
+	log2 "go.guide/add-grpc-service/middleware/log"
+	register2 "go.guide/add-grpc-service/middleware/register"
+	pb2 "go.guide/add-grpc-service/pb"
+	"go.guide/add-grpc-service/service"
+	"go.guide/add-grpc-service/transport"
+	"go.guide/add-grpc-service/transport/grpc"
+	transporthttp "go.guide/add-grpc-service/transport/http"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"net"
+	http1 "net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	port    = flag.Int("port", 50051, "The server port")
+	restful = flag.Int("restful", 8080, "the port to restful serve on")
+)
+
+func main() {
+	flag.Parse()
+	logger := log.With(log2.InitLogger(os.Stdout))
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		return InterruptHandler(ctx)
+	})
+
+	svc := service.NewAddService()
+	svc = log2.LoggingAddServiceMiddleware(logger)(svc)
+	svc = EtcdRegisterAddServiceMiddleware(logger)(svc)
+	endpoints := transport.Endpoints(svc)
+
+	log.With(logger, "level", "info").Log("msg", fmt.Sprintf("grpc server start at 127.0.0.1:%v", *port))
+
+	g.Go(func() error {
+		return ServeGRPC(ctx, &endpoints, fmt.Sprintf("127.0.0.1:%d", *port), log.With(logger, "transport", "GRPC"))
+	})
+
+	g.Go(func() error {
+		return ServeHTTP(ctx, &endpoints, fmt.Sprintf("127.0.0.1:%d", *restful), log.With(logger, "transport", "HTTP"))
+	})
+
+	if err := g.Wait(); err != nil {
+		log.With(logger, "level", "error").Log("error", err)
+	}
+}
+
+// InterruptHandler handles first SIGINT and SIGTERM and returns it as error.
+func InterruptHandler(ctx context.Context) error {
+	interruptHandler := make(chan os.Signal, 1)
+	signal.Notify(interruptHandler, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case sig := <-interruptHandler:
+		return fmt.Errorf("signal received: %v", sig.String())
+	case <-ctx.Done():
+		return errors.New("signal listener: context canceled")
+	}
+}
+
+// ServeGRPC serves gRPC requests.
+func ServeGRPC(ctx context.Context, endpoints *transport.EndpointsSet, addr string, logger log.Logger) error {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	server := transportgrpc.NewGRPCServer(endpoints,
+		logger,
+		opentracinggo.NoopTracer{},
+	)
+	grpcServer := grpc.NewServer()
+	pb2.RegisterAddServiceServer(grpcServer, server)
+	log.With(logger, "level", "info").Log("listen on", addr)
+
+	ch := make(chan error)
+	go func() {
+		ch <- grpcServer.Serve(listener)
+	}()
+
+	select {
+	case err := <-ch:
+		return fmt.Errorf("grpc server: serve: %v", err)
+	case <-ctx.Done():
+		grpcServer.GracefulStop()
+		return errors.New("grpc server: context canceled")
+	}
+}
+
+func ServeHTTP(ctx context.Context, endpoints *transport.EndpointsSet, addr string, logger log.Logger) error {
+	handler := transporthttp.NewHTTPHandler(endpoints,
+		logger,
+		opentracinggo.NoopTracer{},
+	)
+	httpServer := &http1.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+	log.With(logger, "level", "info").Log("http listen on", addr)
+	ch := make(chan error)
+	go func() {
+		ch <- httpServer.ListenAndServe()
+	}()
+	select {
+	case err := <-ch:
+		if errors.Is(err, http1.ErrServerClosed) {
+			return nil
+		}
+		return fmt.Errorf("http server: serve: %v", err)
+	case <-ctx.Done():
+		return httpServer.Shutdown(context.Background())
+	}
+}
+
+func EtcdRegisterAddServiceMiddleware(logger log.Logger) service.AddServiceMiddleware {
+	return func(next service.AddService) service.AddService {
+		r := register2.GetEtcdRegister()
+		if r == nil {
+			log.With(logger, "level", "error").Log("msg", "get register client failed")
+			return next
+		}
+		err := r.Register(etcdv3.Service{Key: register2.ServiceKey, Value: fmt.Sprintf("127.0.0.1:%d", *port)})
+		if err != nil {
+			log.With(logger, "level", "error").Log("msg", "register service failed")
+			return next
+		}
+
+		return next
+	}
+}
