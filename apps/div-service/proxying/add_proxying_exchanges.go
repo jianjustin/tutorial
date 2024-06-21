477de41b1069022bea4e9afe1d3be45655eb3221
jianjustin <janine.jian.chen@gmail.com>
2024-06-21 16:07:26 +0800

    fix bug

--- apps/div-service/proxying/add_proxying_exchanges.go
+++ apps/div-service/proxying/add_proxying_exchanges.go
@@ -1,11 +1,26 @@
 package proxying
 
-import "context"
+import (
+	"context"
+	"go.guide/div-grpc-service/pb"
+)
 
 func _Encode_Grpc_Add_Request(ctx context.Context, req interface{}) (interface{}, error) {
-	return req, nil
+	r := req.(pb.AddRequest)
+	return &pb.AddRequest{A: r.A}, nil
+}
+
+func _Decode_Grpc_Add_Request(ctx context.Context, req interface{}) (interface{}, error) {
+	r := req.(*pb.AddRequest)
+	return pb.AddRequest{A: r.A}, nil
+}
+
+func _Encode_Grpc_Add_Response(ctx context.Context, resp interface{}) (interface{}, error) {
+	r := resp.(*pb.AddResponse)
+	return &pb.AddResponse{V: r.V}, nil
 }
 
 func _Decode_Grpc_Add_Response(ctx context.Context, resp interface{}) (interface{}, error) {
-	return resp, nil
+	r := resp.(*pb.AddResponse)
+	return &pb.AddResponse{V: r.V}, nil
 }
