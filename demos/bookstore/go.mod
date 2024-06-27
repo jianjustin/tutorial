b38fb978807f69e6e2b26cd4e099363d68a68b1b
jianjustin <janine.jian.chen@gmail.com>
2024-06-27 19:07:24 +0800

    feat: add demo bookstore

---
+++ demos/bookstore/go.mod
@@ -0,0 +1,14 @@
+module go.guide/bookstore
+
+go 1.20
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/zeromicro/go-zero v1.6.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ demos/bookstore/internal/handler/addhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"bookstore/internal/logic"
+	"bookstore/internal/svc"
+	"bookstore/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func AddHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.AddReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewAddLogic(r.Context(), svcCtx)
+		resp, err := l.Add(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
